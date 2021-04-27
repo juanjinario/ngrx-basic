3351f043b6b93782432c52da2c5d66ee282265c0
jortiz-belike <jortiz@belikesoftware.com>
2021-04-27 15:35:04 +0200

    first commit

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { ChildComponent } from './components/child/child.component';
+import { GrandchildComponent } from './components/grandchild/grandchild.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ChildComponent,
+    GrandchildComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
